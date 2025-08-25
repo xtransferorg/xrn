811e92084dcedec04a5907bc34d2c43f3f5998db
连永胜 <1732554225@qq.com>
2025-08-25 13:54:55 +0800

    chore: 更新publish.sh脚本，添加yarn发布命令并注释掉不必要的代码

--- publish.sh
+++ publish.sh
@@ -5,17 +5,19 @@ export APP_NAME=$2
 echo "is_prod: $is_prod"
 echo "APP_NAME: $APP_NAME"
 
+yarn publish:from-package
+
 # 将逗号分隔的包名转换为空格分隔
-PACKAGES=${APP_NAME//,/ }
+# PACKAGES=${APP_NAME//,/ }
 
 # nvm install v20.16.0 && nvm use v20.16.0 
 
 # yarn install --no-immutable
 
-if [ $is_prod == "false" ]; then
-    echo "beta"
-    npx xrn-tools batch-publish $PACKAGES --beta
-else
-    echo "prod"
-    npx xrn-tools batch-publish $PACKAGES
-fi
+# if [ $is_prod == "false" ]; then
+#     echo "beta"
+#     npx xrn-tools batch-publish $PACKAGES --beta
+# else
+#     echo "prod"
+#     npx xrn-tools batch-publish $PACKAGES
+# fi
